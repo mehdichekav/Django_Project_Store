6134709e6dfe0113bf1c06cf37c8b56d91b74fac
Mehdi <m.chekave.jazireh@gmail.com>
2021-08-09 21:20:03 +0430

    Complete the section cart

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BaseModel
 
-# Register your models here.
+
+# @admin.register(BaseModel)
+# class CoreAdmin(admin.ModelAdmin):
+#     list_display = ('log_delete', 'is_expired', )
+#
+#     def log_delete(self, request, queryset):
+#         rows = queryset.update(True)
+#         self.rows = True
+#         self.save()
+#
+#     admin.site.add_action(log_delete)
+
+
+
