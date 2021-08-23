296791db5eab896e2f6adf3864ee8eea5eceedd5
Mehdi <m.chekave.jazireh@gmail.com>
2021-08-23 20:02:35 +0430

    app core update core

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class BaseModel(models.Model):
@@ -24,3 +25,6 @@ class TestModel(BaseModel):
     pass
 
 
+class TimeStamp(models.Model):
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
+    update = models.DateTimeField(auto_now=True, verbose_name=_('update'))
